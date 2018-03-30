5630491d467043ea12eeb28cfc72d0085e5c7a31
Monsterthirteen <809609345@qq.com>
2018-03-30 15:56:42 +0800

    add reply; change messageDetails;

--- pages/messageDetails/index.js
+++ pages/messageDetails/index.js
@@ -13,6 +13,7 @@ Page({
         time:'2018-03-19'
       }
     ],
+
     text:[
       {
         paragraph_one:'我们这次主要体验车辆的ESC以及ABS功能，主要通过定圆、双向变 道、绕桩、冰面制动等项目来体验车辆在开启和关闭ESC情况下的状态表现。新款东南DX7升级了ESC和ABS控制单元，搭载了来自万都(mando)的全新MGH-100产品，与原搭载的MGH-80相比，其噪音控制更合理，频率和响应速度也要更快。',
@@ -23,6 +24,15 @@ Page({
         ]
       }
     ],
+
+    imgList:[
+      {
+        url:'../images/collection.png',
+        hoverUrl:'../images/collection_c.png'
+      }
+    ],
+    curIdx: false,
+
     commentDetails:[
       {
         avatarUrl:'../images/bmw.jpg',
@@ -103,5 +113,20 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+
+  toReply:function(){
+    wx.navigateTo({
+      url: '../reply/index',
+      fail:function(){
+        console.log('404')
+      }
+    })
+  },
+
+  chooseThis() {
+    this.setData({
+      curIdx: !this.data.curIdx
+    })
   }
 })
